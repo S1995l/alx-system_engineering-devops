f5909e7586a43d1b757050390774cef9924e8dc4
S1995l <skhosanasl95@gmail.com>
2024-06-04 14:21:48 +0000

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,31 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+This module contains a function that returns the number of
+subscribers for a given subreddit
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """
+    Queries the Reddit API and returns the number of
+    subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: Number of subscribers or 0 if subreddit is invalid.
+    """
+    headers = {'User-Agent': 'Mozilla/5.0'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
+
     if response.status_code == 200:
+        # Parse JSON response
         data = response.json()
-        subscribers = data['data']['subscribers']
+        # Extract number of subscribers from the response data
+        subscribers = data.get("data", {}).get("subscribers", 0)
         return subscribers
-    else:
-        return 0
+    return 0
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,28 @@
 #!/usr/bin/python3
 """
-Script to print hot posts on a given Reddit subreddit.
+This module contains a function that prints the titles of the
+top 10 hottest posts for a specified subreddit
 """
 
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    # Construct the URL for the subreddit's hot posts in JSON format
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-
-    # Define headers for the HTTP request, including User-Agent
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-
-    # Define parameters for the request, limiting the number of posts to 10
-    params = {
-        "limit": 10
-    }
+    """
+    prints the tites of the top 10 hottest posts for a specified subreddit
+    subreddit: (string) - the subreddit to be queried
+    """
+    if subreddit is not None and type(subreddit) is str:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        headers = {"User-Agent": "Aevy21"}
+        params = {"limit": 10}
+        reddit_resp = requests.get(url, params=params, headers=headers,
+                                   allow_redirects=False).json()
+        reddit_response_data = reddit_resp.get("data", {})
+        if reddit_response_data:
+            for data_entry in reddit_response_data:
+                if data_entry == "children":
+                    for entry in reddit_response_data[data_entry]:
+                        print(entry.get("data", {}).get("title", None))
+                    return
+    print(None)
